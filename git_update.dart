0fc5c70f2ccfb9f534e77af79eb9868aba66f435
deanj <deanj7798@gmail.com>
2025-03-14 13:17:33 +0330

    add branch

---
+++ git_update.dart
@@ -0,0 +1,8 @@
+//update github repo on main branch
+// git add .
+// git commit -m "Your commit message here"
+// git push origin main
+
+//go back to previous update on same brach with command hash,
+//git log
+//git reset --hard <commit-hash>
